8f5c7c19e6578d793f4ed3bfff48f6a7520e4a00
gattini0928 <gabrielgattini659@gmail.com>
2026-04-24 10:55:32 -0300

    páginas de formulários criadas

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import home, login, signup, profile, logout
 
 app_name = "core"
 
 urlpatterns = [
-    path("", home, name="home")
+    path("", home, name="home"),
+    path("signup/", signup, name="signup"),
+    path("login/", login, name="login"),
+    path("profile/", profile, name="profile"),
+    path("logout/", logout, name="logout"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, "home.html")
+
+def signup(request):
+    return render(request, "signup.html")
+
+def login(request):
+    return render(request, "login.html")
+
+@login_required
+def profile(request):
+    return render(request, "profile.html")
+
+@login_required
+def logout(request):
+    return redirect("core:login")
